1c7c89bd5d110b6f121b3342c7c8dff87b4e9d75
Nick Yahnke <nick@pitchinteractive.com>
2021-11-23 14:16:14 -0800

    add methodology section

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
 import DataView from "DataView"
-
+import Methodology from 'Methodology'
 const routes = [
   { label: 'Home', path: '/' },
   { label: 'Data', path: '/data', Component: DataView },
   { label: 'Finance Tracker', path: '/finance' },
-  { label: 'Methodology', path: '/methodology' },
+  { label: 'Methodology', path: '/methodology', Component: Methodology },
   { label: 'Research', path: '/research' },
 ]
 
