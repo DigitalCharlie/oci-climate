dcee35e188a095be6d722a980bb7f6992c4c70c1
Nick Yahnke <nick@pitchinteractive.com>
2021-11-22 16:13:33 -0800

    add fossil fuel color scale

--- src/DataView/subcategoryColorScale.js
+++ src/DataView/subcategoryColorScale.js
@@ -1,5 +1,19 @@
 import { scaleOrdinal } from "d3-scale";
 import { schemeCategory10 } from "d3-scale-chromatic";
 
-const colorScale = scaleOrdinal(schemeCategory10);
+const d3colorScale = scaleOrdinal(schemeCategory10);
+
+const fossilFuels = {
+  'Gas': '#f4ebe2',
+  'Oil and Gas': '#cbc6aa',
+  'Oil': '#ffdd52',
+  'Coal': '#4d2907',
+  'Mixed Fossil': '#d8794f',
+}
+const colorScale = (subcategory) => {
+  if (fossilFuels[subcategory]) {
+    return fossilFuels[subcategory];
+  }
+  return d3colorScale(subcategory);
+}
 export default colorScale;
