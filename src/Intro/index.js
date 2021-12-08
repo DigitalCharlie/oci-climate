47f3837678432ae29fda2937c9b4895e53a8e975
Nick Yahnke <nick@pitchinteractive.com>
2021-12-08 11:41:46 -0800

    adjust intro timings & positionings

--- src/Intro/index.js
+++ src/Intro/index.js
@@ -24,6 +24,7 @@ export default function Intro(props) {
   const [showMapBars, setShowMapBars] = useState(false);
   const [mapFilled, setMapFilled] = useState(false);
   const [mapVisible, setMapVisible] = useState(false);
+  const [restOfIntroVisible, setRestOfIntroVisible] = useState(false);
   const data = useDataHook()
 
   const introContainer = useRef()
@@ -49,10 +50,10 @@ export default function Intro(props) {
       setTimeout(() => {
         setP2Visible(true);
         setShowMapBars(true);
-      }, 4000)
+      }, 6000)
       setTimeout(() => {
         setButtonsVisible(true);
-      }, 6000)
+      }, 8000)
     }
   }, [data, introMapSize, collection])
 
@@ -71,6 +72,9 @@ export default function Intro(props) {
             }
           }
         )
+        .on('end', () => {
+          setTimeout(() => setRestOfIntroVisible(true), 300)
+        })
     } else {
       window.document.body.style.overflowY = 'hidden';
     }
@@ -87,8 +91,8 @@ export default function Intro(props) {
       window.removeEventListener('resize', resize)
     }
   },[])
-
-  const [fossilFuelBoxRef, fossilFuelBoxInView, fossilFuelBoxEntry] = useInView({ threshold: 1 })
+  const rootMargin = `0px 0px -${contentHeight / 6}px 0px`
+  const [fossilFuelBoxRef, fossilFuelBoxInView, fossilFuelBoxEntry] = useInView({ threshold: 1, rootMargin })
   const [cleanBoxRef, cleanBoxInView, cleanBoxEntry] = useInView({ threshold: 1 })
   const [cleanBoxRef2, cleanBoxInView2, cleanBoxEntry2] = useInView({ threshold: 1 })
   const [cleanBoxRef3, cleanBoxInView3, cleanBoxEntry3] = useInView({ threshold: 1 })
@@ -101,11 +105,12 @@ export default function Intro(props) {
 
   let expandedHeight =  mobileLayout ? contentHeight * 0.2 : contentHeight * 0.8
   let showingBars = false
+  console.log(fossilFuelBoxEntry)
   if (fossilFuelBoxInView || (fossilFuelBoxEntry && fossilFuelBoxEntry.boundingClientRect.top < 0)) {
     showingBars = true
     fossilFuelBoxHeight = expandedHeight
   }
-  console.log(fossilFuelBoxInView, fossilFuelBoxEntry)
+  // console.log(fossilFuelBoxInView, fossilFuelBoxEntry)
   if (cleanBoxInView || (cleanBoxEntry && cleanBoxEntry.boundingClientRect.top < 0)) {
     cleanBoxHeight = fossilFuelBoxHeight / 2.5
   }
@@ -121,7 +126,7 @@ export default function Intro(props) {
   const fossilFuelOpacity = fossilFuelBoxHeight ? 1 : 0
   const cleanOpacity = cleanBoxHeight ? 1 : 0
 
-  const boxHeights = useSpring({ fossilFuelBoxHeight, cleanBoxHeight, config: { duration: 1000, ease: easeCubic }})
+  const boxHeights = useSpring({ fossilFuelBoxHeight, cleanBoxHeight, config: { duration: 1000, easing: easeCubic }})
   const barWidth = introMapSize ?  (mobileLayout ? introMapSize[0] * 0.3 : introMapSize[0] * 0.1) : 0
 
   let mobileSVGOffset = mobileLayout ? 80 : 0
@@ -149,17 +154,26 @@ export default function Intro(props) {
         <Link to='/data'>Explore the data</Link>
         <div className={classNames('scrollToContinue', {visible: introDismissed && !finalBoxInView})}>Scroll to continue reading</div>
       </div>
-      <div className='restOfIntro' style={{ paddingBottom: contentHeight / 4}}>
-        <section style={{ minHeight: contentHeight * 2}}>
-          <h2 style={{ top: headerHeight}}>Why does international public finance for energy matter？</h2>
-          <p >The International Energy Agency is clear we need to end all finance for new fossil fuel supply and rapidly transition to renewable energy and to stay within 1.5°C of warming and avoid the worst climate impacts.</p>
+      <div className='restOfIntro' style={{ paddingBottom: contentHeight / 2, opacity: restOfIntroVisible ? 1 : 0}}>
+        <section style={{ minHeight: contentHeight * 3, paddingBottom: contentHeight / 2}}>
+         <div>
+            <h2 style={{ top: headerHeight}}>Why does international public finance for energy matter？</h2>
+
+           <p>
+              The International Energy Agency is clear we need to end all finance for new fossil fuel supply and rapidly transition to renewable energy and to stay within 1.5°C of warming and avoid the worst climate impacts.
+            </p>
+          </div>
           <p ref={fossilFuelBoxRef}>Despite this, G20 countries are still financing at least USD <strong className='large'>$63 billion a year</strong> for <span className='fossilFuel'>fossil fuel</span> <strong>projects</strong> through their export credit agencies, development finance institutions, and multilateral development banks (2018-2020 average). </p>
           <p ref={cleanBoxRef}>This is <strong className='large'>2.5x their support</strong> for <span className='clean'>renewable energy</span>.</p>
           <p>This money – provided as loans, guarantees, equity, and grants — has an outsized impact on energy systems. Public finance often comes with below-market rates, technical capacity, and decreased financial risks that make projects much more likely to go forward — something that is increasingly influential as the fossil fuel industry faces unprecedented global headwinds. We need this $63 billion a year in public finance for fossil fuels  —  and more — to support a just transition to renewable energy instead. </p>
         </section>
-        <section  style={{ minHeight: contentHeight * 2}}>
-          <h2 style={{ top: headerHeight}}>How do we get public finance out for fossils?</h2>
-          <p ref={cleanBoxRef2}>Momentum is building to finally make <strong className='large'>public finance fossil free</strong>. </p>
+        <section  style={{ minHeight: contentHeight * 1.5}}>
+          <div>
+            <h2 style={{ top: headerHeight}}>How do we get public finance out for fossils?</h2>
+            <p ref={cleanBoxRef2}>
+              Momentum is building to finally make <strong className='large'>public finance fossil free</strong>.
+            </p>
+          </div>
           <p ref={cleanBoxRef3}>As of 2021, almost all G20 countries have policies to end their coal finance. And six G20 countries, along with 33 other countries and institutions, signed a joint commitment at COP26 to end their international support for oil and gas by 2022 as well.</p>
           <p ref={finalBoxRef}>We’re tracking G20 countries’ and MDBs’ implementation of these promises into policy here. Visit our research and action page here to learn more and help make sure governments <strong className='large'>#StopFundingFossils</strong>.  </p>
         </section>
