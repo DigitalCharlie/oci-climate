b1a85287b9c1762fba800431cdb23631e2ab4151
Nick Yahnke <nick@pitchinteractive.com>
2021-12-22 11:54:22 -0600

    fix resources image sizes

--- src/Research/index.js
+++ src/Research/index.js
@@ -1,6 +1,7 @@
 import './styles.scss'
 import ReactMarkdown from 'react-markdown'
 import { useState, useEffect } from 'react'
+import classNames from 'classnames'
 export default function Research(props) {
   const { headerHeight } = props
   const [researchMarkdown, setResearchMarkdown] = useState('')
@@ -12,9 +13,18 @@ export default function Research(props) {
   const imageURI = (src, alt, title) => {
     return `${process.env.PUBLIC_URL}/${src}`
   }
+  const c = {
+    img: (node, inline, className, children, ...props) => {
+      return <div className='imageContainer'><img src={node.src} alt={node.alt} title={node.title}  /></div>
+    },
+    p: (node, inline, className, children, ...props) => {
+      const hasImage = node.children.some(child => child && child.props && child.props.node && child.props.node.tagName === 'img')
+      return <p className={classNames({hasImage})}>{node.children}</p>
+    }
+  }
   return (
     <div className='Research' style={{ marginTop: headerHeight}}>
-      <ReactMarkdown linkTarget='_blank' transformImageUri={imageURI}>{researchMarkdown}</ReactMarkdown>
+      <ReactMarkdown components={c} linkTarget='_blank' transformImageUri={imageURI}>{researchMarkdown}</ReactMarkdown>
     </div>
   )
 }
