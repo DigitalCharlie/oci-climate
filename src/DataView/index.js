7ae47b82afbf58e76a1decc127e037f8145e40e7
Digital Charlie <furman.jr@gmail.com>
2022-10-31 18:29:58 -0700

    fixing order

--- src/DataView/index.js
+++ src/DataView/index.js
@@ -11,10 +11,10 @@ import Checkbox from './Checkbox'
 import InfoIcon from './InfoIcon'
 import YearPicker from './YearPicker'
 import { Link } from 'react-router-dom'
+import { finalYear } from 'App'
 const ExLink = ({href, children}) => {
   return <a href={href} target="_blank" rel="noopener noreferrer">{children}</a>
 }
-import { finalYear } from 'App'
 const energyTypes = ['Fossil Fuel', 'Clean', 'Other']
 export const colors = {
   // OLD COLORS
